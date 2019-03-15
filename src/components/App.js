92ae2d9ad73b38fbbc393c62978767fe8cc430d8
Ryan Thorn <ryandthorn@gmail.com>
2019-03-15 15:36:12 -0700

    Wrap App in Router, add mock Dashboard route

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Header from "./Header";
 import Landing from "./Landing";
 import Footer from "./Footer";
+// import Dashboard from "./Dashboard";
 
 export default function App() {
   return (
-    <div>
-      <Header loggedIn="false" />
-      <Landing />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header loggedIn="false" />
+        <Route exact path="/" component={Landing} />
+        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
