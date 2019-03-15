0cd51dac2062ea720d7f678a783e3fb2277e558b
Ryan Thorn <ryandthorn@gmail.com>
2019-03-15 14:58:21 -0700

    Refactor App to functional comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
 import Header from "./Header";
 import Landing from "./Landing";
 import Footer from "./Footer";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header loggedIn="false" />
-        <Landing />
-        <Footer />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Header loggedIn="false" />
+      <Landing />
+      <Footer />
+    </div>
+  );
 }
-
-export default App;
