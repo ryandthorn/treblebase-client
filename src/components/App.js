c1079a68af9984c7c1bc80d8cf0df32bcaba70ce
Ryan Thorn <ryandthorn@gmail.com>
2019-03-15 16:29:51 -0700

    Add Dashboard comp, test, style

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header";
 import Landing from "./Landing";
 import Footer from "./Footer";
-// import Dashboard from "./Dashboard";
+import Dashboard from "./Dashboard";
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <div>
         <Header loggedIn="false" />
         <Route exact path="/" component={Landing} />
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        <Route exact path="/dashboard" component={Dashboard} />
         <Footer />
       </div>
     </Router>
