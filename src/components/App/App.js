8dc1b5626206764d5aa10a7fd88c4f78aa25a6bc
Ryan Thorn <ryandthorn@gmail.com>
2019-04-06 08:19:30 -0700

    Move parent Router to index

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Landing from "../../views/Landing/Landing";
@@ -13,26 +13,24 @@ import NoMatch from "../../views/NoMatch/NoMatch";
 
 export default function App() {
   return (
-    <Router>
-      <div className="app">
-        <header>
-          <Switch>
-            <Route path="/dashboard" component={NavBar} />
-            <Route path="/profile" component={NavBar} />
-            <Route path="/" component={AuthBar} />
-          </Switch>
-        </header>
-        <main>
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/profile" component={Profile} />
-            <Route exact path="/register" component={Registration} />
-            <Route exact path="/login" component={Login} />
-            <Route component={NoMatch} />
-          </Switch>
-        </main>
-      </div>
-    </Router>
+    <div className="app">
+      <header>
+        <Switch>
+          <Route path="/dashboard" component={NavBar} />
+          <Route path="/profile" component={NavBar} />
+          <Route path="/" component={AuthBar} />
+        </Switch>
+      </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/register" component={Registration} />
+          <Route exact path="/login" component={Login} />
+          <Route component={NoMatch} />
+        </Switch>
+      </main>
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import store from "./store";
 import "./normalize.css";
 import App from "./components/App/App";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
