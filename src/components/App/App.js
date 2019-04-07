c279750100379bdf0a2553544b6ccd94a11e1bce
Ryan Thorn <ryandthorn@gmail.com>
2019-04-07 10:39:57 -0700

    Connect App, NavBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import "./App.css";
 
 import Landing from "../../views/Landing/Landing";
@@ -11,26 +12,36 @@ import AuthBar from "../AuthBar/AuthBar";
 import NavBar from "../NavBar/NavBar";
 import NoMatch from "../../views/NoMatch/NoMatch";
 
-export default function App() {
-  return (
-    <div className="app">
-      <header>
-        <Switch>
-          <Route path="/dashboard" component={NavBar} />
-          <Route path="/profile" component={NavBar} />
-          <Route path="/" component={AuthBar} />
-        </Switch>
-      </header>
-      <main>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/profile" component={Profile} />
-          <Route exact path="/register" component={Registration} />
-          <Route exact path="/login" component={Login} />
-          <Route component={NoMatch} />
-        </Switch>
-      </main>
-    </div>
-  );
+export class App extends React.Component {
+  render() {
+    // console.log({ AppProps: this.props });
+    return (
+      <div className="app">
+        <header>
+          <Switch>
+            <Route path="/dashboard" component={NavBar} />
+            <Route path="/profile" component={NavBar} />
+            <Route path="/" component={AuthBar} />
+          </Switch>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/register" component={Registration} />
+            <Route exact path="/login" component={Login} />
+            <Route component={NoMatch} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  user: state.user,
+  loggedIn: state.loggedIn
+});
+
+export default connect(mapStateToProps)(App);
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
+import AuthBar from "../AuthBar/AuthBar";
 import "./NavBar.css";
-import headshot from "./mock-headshot.jpg";
+import defaultPic from "./trebleclef.svg";
 
-export default function NavBar() {
-  return (
-    <div className="navbar">
-      <section className="logo">
-        <div className="logo-ph">Logo</div>
-      </section>
+export class NavBar extends React.Component {
+  render() {
+    if (!this.props.user) {
+      return <AuthBar />;
+    }
+    return (
+      <div className="navbar">
+        <section className="logo">
+          <div className="logo-ph">Logo</div>
+        </section>
 
-      <section className="navigation">
-        <div className="wrap__headshot">
-          <img className="headshot" src={headshot} alt="Your headshot" />
-        </div>
-        <div className="wrap__menu">
-          <img
-            className="menu"
-            src="https://img.icons8.com/material/24/000000/menu.png"
-            alt="View menu"
-          />
-        </div>
-      </section>
-    </div>
-  );
+        <section className="navigation">
+          <div className="wrap__headshot">
+            <img
+              className="headshot"
+              src={this.props.user.headshot || defaultPic}
+              alt="Your headshot"
+            />
+          </div>
+          <div className="wrap__menu">
+            <img
+              className="menu"
+              src="https://img.icons8.com/material/24/000000/menu.png"
+              alt="View menu"
+            />
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  user: state.user
+});
+
+export default connect(mapStateToProps)(NavBar);
