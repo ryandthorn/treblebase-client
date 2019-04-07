664a0dc99c01b972ab0b68bfc0f9ce2dee9449dd
Ryan Thorn <ryandthorn@gmail.com>
2019-04-06 18:08:54 -0700

    Add fetchUser actions; get fetchPosts working

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,10 @@ export const FETCH_POSTS = "FETCH_POSTS";
 export const FETCH_POSTS_SUCCESS = "FETCH_POSTS_SUCCESS";
 export const FETCH_POSTS_FAILURE = "FETCH_POSTS_FAILURE";
 
+export const FETCH_USER = "FETCH_USER";
+export const FETCH_USER_SUCCESS = "FETCH_USER_SUCCESS";
+export const FETCH_USER_FAILURE = "FETCH_USER_FAILURE";
+
 export const USER_LOGIN = "USER_LOGIN";
 export const USER_LOGIN_SUCCESS = "USER_LOGIN_SUCCESS";
 export const USER_LOGIN_FAILURE = "USER_LOGIN_FAILURE";
@@ -12,15 +16,22 @@ export const USER_REGISTRATION = "USER_REGISTRATION";
 export const USER_REGISTRATION_SUCCESS = "USER_REGISTRATION_SUCCESS";
 export const USER_REGISTRATION_FAILURE = "USER_REGISTRATION_FAILURE";
 
-export const fetchPosts = queryString => dispatch => {
-  fetch(`${API_BASE_URL}/posts/${queryString}`, {
-    headers: new Headers({ Authorization: `Bearer ${localStorage.jwt}` })
-  })
-    .then(res => {
-      !res.ok ? Promise.reject(res.statusText) : res.json();
+export const fetchPosts = () => dispatch => {
+  const jwt = localStorage.getItem("jwt");
+  const auth = {
+    headers: new Headers({
+      Authorization: `Bearer ${jwt}`
     })
-    .then(posts => {
-      dispatch(fetchPostsSuccess(posts));
+  };
+  fetch(`${API_BASE_URL}/posts/`, auth)
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(resJson => {
+      dispatch(fetchPostsSuccess(resJson));
     })
     .catch(err => {
       console.error(err);
@@ -28,13 +39,45 @@ export const fetchPosts = queryString => dispatch => {
     });
 };
 
-export const fetchPostsSuccess = posts => ({
+export const fetchPostsSuccess = resJson => ({
   type: FETCH_POSTS_SUCCESS,
-  posts
+  posts: resJson.posts
 });
 
 export const fetchPostsFailure = err => ({
   type: FETCH_POSTS_FAILURE,
+  err
+});
+
+export const fetchUser = () => dispatch => {
+  const jwt = localStorage.getItem("jwt");
+  const auth = {
+    headers: new Headers({
+      Authorization: `Bearer ${jwt}`
+    })
+  };
+  fetch(`${API_BASE_URL}/users`, auth)
+    .then(res => {
+      if (!res.ok) {
+        return Promise.reject(res.statusText);
+      }
+      return res.json();
+    })
+    .then(user => {
+      dispatch(fetchUserSuccess(user));
+    })
+    .catch(err => {
+      console.error(err);
+      dispatch(fetchUserFailure(err));
+    });
+};
+export const fetchUserSuccess = user => ({
+  type: FETCH_USER_SUCCESS,
+  user
+});
+
+export const fetchUserFailure = err => ({
+  type: FETCH_USER_FAILURE,
   err
 });
 
@@ -43,7 +86,7 @@ export const userLogin = userInfo => dispatch => {
     email: userInfo.email,
     password: userInfo.password
   });
-  return fetch(`${API_BASE_URL}/auth/login`, {
+  fetch(`${API_BASE_URL}/auth/login`, {
     method: "POST",
     body: data,
     headers: { "Content-Type": "application/json" }
@@ -55,6 +98,7 @@ export const userLogin = userInfo => dispatch => {
       return res.json();
     })
     .then(resJson => {
+      dispatch(fetchUser());
       dispatch(userLoginSuccess(resJson.authToken));
     })
     .catch(err => {
@@ -87,7 +131,10 @@ export const userRegistration = userInfo => dispatch => {
       return res.json();
     })
     .then(resJson => {
-      dispatch(userRegistrationSuccess(resJson));
+      // dispatch(userRegistrationSuccess(resJson));
+      dispatch(
+        userLogin({ email: userInfo.email, password: userInfo.password })
+      );
     })
     .catch(err => {
       console.error(err);
