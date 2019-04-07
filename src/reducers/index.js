d5b588c76f875d4ebd9b1fffa903530a19d8fad9
Ryan Thorn <ryandthorn@gmail.com>
2019-04-06 18:06:56 -0700

    Refactor into one reducer - needs splitting

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,49 @@
-// import * as actions from "../actions";
-import usersReducer from "./users";
-import authReducer from "./auth";
+import {
+  FETCH_POSTS,
+  FETCH_POSTS_SUCCESS,
+  FETCH_USER,
+  FETCH_USER_SUCCESS,
+  USER_LOGIN_SUCCESS
+} from "../actions";
+// import usersReducer from "./users";
+// import postsReducer from "./posts";
 
 const initialState = {
-  posts: [],
-  user: null
+  posts: null,
+  user: null,
+  fetchingPost: false,
+  fetchingUser: false,
+  loggedIn: false
 };
 
+// export const indexReducer = (state = initialState, action) => {
+//   return {
+//     user: usersReducer(state, action),
+//     posts: postsReducer(state, action)
+//   };
+// };
+
 export const indexReducer = (state = initialState, action) => {
-  return {
-    user: usersReducer(state, action),
-    auth: authReducer(state, action)
-  };
+  // console.log({ action, state });
+  switch (action.type) {
+    case FETCH_POSTS:
+      return Object.assign({}, state, { fetchingPost: true });
+    case FETCH_USER:
+      return Object.assign({}, state, { fetchingUser: true });
+    case FETCH_POSTS_SUCCESS:
+      return Object.assign({}, state, {
+        fetchingPost: false,
+        posts: [...action.posts]
+      });
+    case USER_LOGIN_SUCCESS:
+      localStorage.setItem("jwt", action.jwt);
+      return Object.assign({}, state, { loggedIn: true });
+    case FETCH_USER_SUCCESS:
+      return Object.assign({}, state, {
+        fetchingUser: false,
+        user: action.user
+      });
+    default:
+      return state;
+  }
 };
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,14 +1,24 @@
 import {
-  USER_REGISTRATION,
-  USER_REGISTRATION_SUCCESS,
-  USER_REGISTRATION_FAILURE
+  // USER_REGISTRATION_SUCCESS,
+  FETCH_USER,
+  FETCH_USER_SUCCESS,
+  USER_LOGIN_SUCCESS
 } from "../actions";
 
-const usersReducer = (state = undefined, action) => {
+const usersReducer = (state, action) => {
   switch (action.type) {
-    case USER_REGISTRATION_SUCCESS:
+    // case USER_REGISTRATION_SUCCESS:
+    //   return Object.assign({}, state, {
+    //     user: action.userInfo
+    //   });
+    case USER_LOGIN_SUCCESS:
+      localStorage.setItem("jwt", action.jwt);
+      return state;
+    case FETCH_USER:
+      return Object.assign({}, state, { fetchingUser: true });
+    case FETCH_USER_SUCCESS:
       return Object.assign({}, state, {
-        user: action.userInfo
+        user: action.user
       });
     default:
       return state;
