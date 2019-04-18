d80eba296236a7d6677b5254da43cc4b2c995646
Ryan Thorn <ryandthorn@gmail.com>
2019-04-18 09:00:35 -0700

    Move CompDidMount => render in Dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -7,7 +7,7 @@ import { fetchPosts } from "../../actions/posts";
 import { fetchUser } from "../../actions/users";
 
 export class Dashboard extends React.Component {
-  componentDidMount() {
+  render() {
     const jwt = localStorage.getItem("jwt");
     if (!jwt) {
       return <Redirect to="/login" />;
@@ -16,9 +16,6 @@ export class Dashboard extends React.Component {
       this.props.dispatch(fetchUser());
       this.props.dispatch(fetchPosts());
     }
-  }
-
-  render() {
     return (
       <div className="dashboard">
         <FilterPosts />
