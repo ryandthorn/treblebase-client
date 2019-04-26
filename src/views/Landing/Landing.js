693747cae714f2087e0d1382cb94e3821a00f4c1
Ryan Thorn <ryandthorn@gmail.com>
2019-04-26 07:42:21 -0700

    Implement basic demo user

--- src/views/Landing/Landing.js
+++ src/views/Landing/Landing.js
@@ -1,33 +1,50 @@
 import React from "react";
 import "./Landing.css";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import Demo from "../../components/Demo";
+import { connect } from "react-redux";
 
-export default function Landing() {
-  return (
-    <section className="landing">
-      <section className="hero">
-        <img
-          className="img__logo"
-          src="https://st2.depositphotos.com/1747885/7315/v/950/depositphotos_73153169-stock-illustration-logo-treble-clef.jpg"
-          alt="Logo"
-        />
-        <h1>TrebleBase</h1>
-        <h2>Connecting classical musicians to performance opportunities</h2>
-      </section>
+export class Landing extends React.Component {
+  render() {
+    if (this.props.users.loggedIn === true) {
+      return <Redirect to="/dashboard" />;
+    }
+    return (
+      <section className="landing">
+        <section className="hero">
+          <img
+            className="img__logo"
+            src="https://st2.depositphotos.com/1747885/7315/v/950/depositphotos_73153169-stock-illustration-logo-treble-clef.jpg"
+            alt="Logo"
+          />
+          <h1>TrebleBase</h1>
+          <h2>Connecting classical musicians to performance opportunities</h2>
+        </section>
 
-      <section className="register">
-        <h2>Become a part of our community!</h2>
-        <Link to="/register" className="Link__register">
-          Get Started
-        </Link>
-      </section>
+        <section className="demo">
+          <Demo />
+        </section>
 
-      <section className="login">
-        <h3>Already a member?</h3>
-        <Link to="/login" className="Link__login">
-          Log In
-        </Link>
+        <section className="register">
+          <h2>Become a part of our community!</h2>
+          <Link to="/register" className="Link__register">
+            Get Started
+          </Link>
+        </section>
+
+        <section className="login">
+          <h3>Already a member?</h3>
+          <Link to="/login" className="Link__login">
+            Log In
+          </Link>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  users: state.users
+});
+
+export default connect(mapStateToProps)(Landing);
