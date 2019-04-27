caf8887d5725ff6fd5c55a01a15179555e7c0f2b
Ryan Thorn <ryandthorn@gmail.com>
2019-04-27 08:59:21 -0700

    Add heroku url to config

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://treblebase-server.herokuapp.com/api" ||
+  "http://localhost:8080/api";
